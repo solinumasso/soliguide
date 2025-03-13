66d742a073094a9f7fd5aad14b5f7deb9d4825de
Yassine R. <riffi.yassine@gmail.com>
2025-03-13 12:25:36 +0100

    fix(api): fix origin request for pictures #62

--- packages/api/src/_models/config/constants/CONFIG.const.ts
+++ packages/api/src/_models/config/constants/CONFIG.const.ts
@@ -23,9 +23,25 @@ import envSchema from "env-schema";
 import { ENV_SCHEMA } from "./ENV_SCHEMA.const";
 
 import { Config } from "../interfaces";
+import { resolve } from "path";
+import { existsSync } from "fs";
+
+const dotenvConfig = (() => {
+  // If we are in test mode only in local
+  if (process.env.NODE_ENV === "test" && !process.env.DOCKER_ENV) {
+    const testEnvPath = resolve(process.cwd(), ".env.test");
+
+    if (existsSync(testEnvPath)) {
+      return {
+        path: testEnvPath,
+      };
+    }
+  }
+  return true;
+})();
 
 // If values are missing in the '.env' file and no default values are provided for those in the 'ENV_SCHEMA', the app will automatically crash
 export const CONFIG = envSchema<Config>({
   schema: ENV_SCHEMA,
-  dotenv: true,
+  dotenv: dotenvConfig,
 });
