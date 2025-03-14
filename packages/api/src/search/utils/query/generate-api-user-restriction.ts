bf8e4ebb326efac272472ad95037048e83ec2e39
Yassine R. <riffi.yassine@gmail.com>
2025-03-14 14:07:17 +0100

    fix(api): fix number of results for api users  #322

--- packages/api/src/search/utils/query/generate-api-user-restriction.ts
+++ packages/api/src/search/utils/query/generate-api-user-restriction.ts
@@ -21,59 +21,58 @@
 import { RootQuerySelector } from "mongoose";
 import {
   ApiPlace,
+  CommonUser,
   CountryCodes,
   getAllowedTerritories,
-  PlaceType,
   SoliguideCountries,
 } from "@soliguide/common";
 import { UserPopulateType } from "../../../_models";
+
+const createPositionRequestForCountry = (
+  user: Pick<CommonUser, "areas" | "status">,
+  country: SoliguideCountries
+) => {
+  const allowedTerritories = getAllowedTerritories(user, country);
+
+  return {
+    $or: [
+      {
+        "position.departmentCode": { $in: allowedTerritories },
+        "position.country": country,
+      },
+      {
+        "parcours.position.departmentCode": { $in: allowedTerritories },
+        "parcours.position.country": country,
+      },
+    ],
+  };
+};
 
 export const generateApiUserRestriction = (
   nosqlQuery: RootQuerySelector<ApiPlace>,
   user: UserPopulateType
 ): RootQuerySelector<ApiPlace> => {
-  const apiUserRestriction: RootQuerySelector<ApiPlace> = {
-    placeType: PlaceType.PLACE,
-  };
+  const apiUserRestriction: RootQuerySelector<ApiPlace> = {};
 
   const userAreasCountries = Object.keys(user.areas ?? {});
 
+  // One country only
   if (userAreasCountries.length === 1) {
-    const allowedTerritories = getAllowedTerritories(
-      user,
-      userAreasCountries[0] as SoliguideCountries
+    const country = userAreasCountries[0] as SoliguideCountries;
+    const conditions = createPositionRequestForCountry(user, country);
+    Object.assign(apiUserRestriction, conditions);
+  }
+  // Multiple countries
+  else if (userAreasCountries.length > 1) {
+    const countriesOr = userAreasCountries.map((country) =>
+      createPositionRequestForCountry(user, country as SoliguideCountries)
     );
-
-    apiUserRestriction["position.departmentCode"] = {
-      $in: allowedTerritories,
-    };
-    apiUserRestriction["position.country"] =
-      userAreasCountries[0] as SoliguideCountries;
-  } else if (userAreasCountries.length > 1) {
-    const $or = [];
-
-    for (const country of Object.keys(user.areas ?? {})) {
-      const allowedTerritories = getAllowedTerritories(
-        user,
-        country as SoliguideCountries
-      );
-
-      $or.push({
-        "position.departmentCode": {
-          $in: allowedTerritories,
-        },
-        "position.country": country as SoliguideCountries,
-      });
-    }
-
-    apiUserRestriction.$or = $or;
-  } else {
-    const allowedTerritories = getAllowedTerritories(user, CountryCodes.FR);
-
-    apiUserRestriction["position.departmentCode"] = {
-      $in: allowedTerritories,
-    };
-    apiUserRestriction["position.country"] = CountryCodes.FR;
+    apiUserRestriction.$or = countriesOr;
+  }
+  // Default: no country set, we use France
+  else {
+    const conditions = createPositionRequestForCountry(user, CountryCodes.FR);
+    Object.assign(apiUserRestriction, conditions);
   }
 
   return { $and: [apiUserRestriction, nosqlQuery] };
