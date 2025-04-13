b169d3563dbed7256ec025075501b4895685d3df
Yassine R. <riffi.yassine@gmail.com>
2025-04-14 00:04:19 +0200

    fix(location-api): fix cache unit test & upgrade nest #509

--- packages/location-api/src/app.controller.ts
+++ packages/location-api/src/app.controller.ts
@@ -1,3 +1,23 @@
+/*
+ * Soliguide: Useful information for those who need it
+ *
+ * SPDX-FileCopyrightText: © 2025 Solinum
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import { Controller, Get } from "@nestjs/common";
 import { HealthCheck } from "@nestjs/terminus";
 
@@ -9,7 +29,7 @@ export class AppController {
   @HealthCheck()
   async check() {
     return {
-      message: "Hello! Welcome to Location API",
+      message: "Hello! Welcome to Location API from Soliguide",
       timestamp: new Date().toISOString(),
     };
   }
