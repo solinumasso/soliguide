63497633a26765b2a7624dd47b9fee0472a375e7
Younes <116138482+Younes-Kch@users.noreply.github.com>
2025-05-07 14:19:45 +0200

    fix(web-app): use another google map link #610

--- packages/web-app/src/lib/client/geo.ts
+++ packages/web-app/src/lib/client/geo.ts
@@ -46,5 +46,5 @@ export const getGeolocation = (): Promise<GeolocationPosition> =>
   });
 
 export const getMapLink = (address: string): string => {
-  return `https://www.google.com/maps/search/?q=${encodeURIComponent(address)}`;
+  return `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)}`;
 };
