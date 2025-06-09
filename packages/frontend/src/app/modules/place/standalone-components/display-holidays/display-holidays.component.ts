30e4692783e4828d99b72e7868dc90bd908e6a17
Kechiche Younes <younes@solinum.org>
2025-06-09 15:37:44 +0200

    feat(frontend): make temp-info and holiday standalone components #653

--- packages/frontend/src/app/modules/place/standalone-components/display-holidays/display-holidays.component.ts
+++ packages/frontend/src/app/modules/place/standalone-components/display-holidays/display-holidays.component.ts
@@ -28,8 +28,12 @@ import { Search } from "../../../search/interfaces";
 import { Subscription } from "rxjs";
 import { HolidaysService } from "../../services/holidays.service";
 import { Place } from "../../../../models";
+import { CommonModule } from "@angular/common";
+import { TranslatePipe } from "@ngx-translate/core";
 
 @Component({
+  standalone: true,
+  imports: [CommonModule, TranslatePipe],
   selector: "app-display-holidays",
   templateUrl: "./display-holidays.component.html",
   styleUrls: ["./display-holidays.component.scss"],
