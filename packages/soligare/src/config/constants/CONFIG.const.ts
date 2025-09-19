9022c5b0e4f1d2a993c2c92ddddcbcf2785399ec
SnoAke <pierre.blanchais@solinum.org>
2025-09-19 18:58:06 +0200

    fix(soligare): fix soligare connection #905

--- packages/soligare/src/config/constants/CONFIG.const.ts
+++ packages/soligare/src/config/constants/CONFIG.const.ts
@@ -24,7 +24,9 @@ export const CONFIG_VALIDATOR = Joi.object({
   PORT: Joi.number().default(3003),
   NODE_ENV: Joi.string().valid('prod', 'test', 'local').default('local'),
   SOLIGARE_SENTRY_DSN: Joi.string().uri(),
-  POSTGRES_EXTERNAL_URI: Joi.string().default(
-    'postgres://postgres:postgres@localhost:5432/postgres',
-  ),
+  POSTGRES_EXTERNAL_USERNAME: Joi.string().default('postgres'),
+  POSTGRES_EXTERNAL_PASSWORD: Joi.string().default('postgres'),
+  POSTGRES_EXTERNAL_HOST: Joi.string().default('localhost'),
+  POSTGRES_EXTERNAL_PORT: Joi.number().default(5432),
+  POSTGRES_EXTERNAL_DATABASE: Joi.string().default('postgres'),
 });
