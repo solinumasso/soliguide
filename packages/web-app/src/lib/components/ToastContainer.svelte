52835144c3547e4b35bdeb7ebe0e2611ee817c25
David Lambert <david@lacabanedigitale.fr>
2025-10-10 14:57:10 +0200

    feat(web-app): add toast component #956

---
+++ packages/web-app/src/lib/components/ToastContainer.svelte
@@ -0,0 +1,79 @@
+<!--
+Soliguide: Useful information for those who need it
+
+SPDX-FileCopyrightText: © 2024 Solinum
+
+SPDX-License-Identifier: AGPL-3.0-only
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+<script lang="ts">
+  import { Toast } from '@soliguide/design-system';
+  import { fly } from 'svelte/transition';
+  import { removeToast, toasts } from '$lib/toast/toast.store';
+
+  const createVerticalFlyConfig = (offset: number) => ({
+    duration: 200,
+    // eslint-disable-next-line id-length
+    y: offset
+  });
+
+  const toastEnterTransition = createVerticalFlyConfig(20);
+  const toastLeaveTransition = createVerticalFlyConfig(-20);
+</script>
+
+<div class="toast-container">
+  {#each $toasts as toast (toast.id)}
+    {@const { id, variant, description, withIcon, dismissible, autoDismiss, showLoader, loaderDuration } = toast}
+    <div class="toast-wrapper" in:fly={toastEnterTransition} out:fly={toastLeaveTransition}>
+      <Toast
+        variant={variant}
+        description={description}
+        withIcon={withIcon}
+        dismissible={dismissible}
+        autoDismiss={autoDismiss}
+        showLoader={showLoader}
+        loaderDuration={loaderDuration}
+        on:close={() => removeToast(id)}
+      />
+    </div>
+  {/each}
+</div>
+
+<style lang="scss">
+  .toast-container {
+    position: fixed;
+    bottom: calc(var(--spacingLG) + var(--menu-height, 0px));
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacingXS);
+    z-index: 1000;
+  }
+
+  @media screen and (max-width: 768px) {
+    .toast-container {
+      left: 50%;
+      right: auto;
+      transform: translateX(-50%);
+      bottom: calc(var(--spacingSM) + var(--menu-height, 0px));
+      align-items: center;
+    }
+  }
+
+  .toast-wrapper {
+    display: flex;
+  }
+</style>
