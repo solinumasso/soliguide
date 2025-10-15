eb068947f19852ae8404354edf6a39a92938c36e
Kechiche Younes <younes@solinum.org>
2025-10-15 12:27:39 +0200

    feat(location-api): reduce cache duration update

--- packages/location-api/src/config/constants/TTL.const.ts
+++ packages/location-api/src/config/constants/TTL.const.ts
@@ -18,8 +18,11 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+// 1 day = 24 hours * 60 minutes * 60 seconds * 1000 ms
 const DAY_IN_MS = 24 * 60 * 60 * 1000;
-const MONTH_30_IN_MS = 30 * DAY_IN_MS;
+
+// 1 month = 30 days * 24 hours * 60 minutes * 60 seconds * 1000 ms
+export const MONTH_30_IN_MS = 30 * DAY_IN_MS;
 
 // 3 months = 3 * 30 days * 24 hours * 60 minutes * 60 seconds * 1000 ms
 export const THREE_MONTHS_IN_MS = 3 * MONTH_30_IN_MS;
