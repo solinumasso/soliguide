d752ee0377343b323a61c0ca6a0d7d4b98c8e1ac
David Lambert <david@lacabanedigitale.fr>
2025-10-22 17:35:25 +0200

    feat: add limit on favorites #957

--- packages/common/src/favorites/index.ts
+++ packages/common/src/favorites/index.ts
@@ -18,15 +18,5 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-import { body } from "express-validator";
-import { placeTypeDto } from "./placeType.dto";
 
-export const lookupDto = [
-  body("ids")
-    .exists()
-    .bail()
-    .isArray({ min: 1 }),
-  body("ids.*").isInt({ min: 1 }),
-  
-  ...placeTypeDto,
-];
+export const FAVORITES_LIMIT = 20;
