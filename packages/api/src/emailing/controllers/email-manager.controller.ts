de8582f12979e4db0e547d5751a8e0a82beecb7e
Yassine R. <riffi.yassine@gmail.com>
2025-11-13 23:45:51 +0100

    fix(api): delete useless code, fix generation of emails #1

--- packages/api/src/emailing/controllers/email-manager.controller.ts
+++ packages/api/src/emailing/controllers/email-manager.controller.ts
@@ -18,14 +18,10 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-import type { FilterQuery, QueryOptions } from "mongoose";
 import { CAMPAIGN_DEFAULT_NAME } from "@soliguide/common";
 import { mgGetEvent } from "../services/mailgun.service";
 import {
-  countEmailsManage,
-  findEmails,
   findEmailsToUpdate,
-  findOne,
   updateManyEmailCampaign,
   updatOneEmailCampaign,
 } from "../services/email-campaign.service";
@@ -33,8 +29,6 @@ import { getEventType } from "../utils/getEventType";
 import {
   MG_EVENT_STRING_SORTED,
   CampaignEmailNameToSync,
-  type CampaignEmails,
-  type UserPopulateType,
   type User,
 } from "../../_models";
 
@@ -43,64 +37,6 @@ import { logger } from "../../general/logger";
 import { updateUser } from "../../user/services";
 import { PosthogClient } from "../../analytics/services";
 import { TRACKED_EVENTS } from "../../analytics/constants";
-import { generateSearchOptions, parseTerritories } from "../../search/utils";
-
-export const searchEmail = async (searchEmail: any, user: UserPopulateType) => {
-  const query: FilterQuery<CampaignEmails> = {};
-
-  if (searchEmail.campaigns) {
-    query.campaign = { $in: searchEmail.campaigns };
-  }
-
-  if (searchEmail.lastStatus) {
-    if (searchEmail.lastStatus === "BOUNCED_PERM") {
-      query.lastStatus = {
-        $in: ["BOUNCED_PERM", "BOUNCED_TEMP", "REJECTED"],
-      };
-    } else {
-      query.lastStatus = searchEmail.lastStatus;
-    }
-  }
-
-  if (searchEmail.emailType) {
-    query.emailType = searchEmail.emailType;
-  }
-
-  if (typeof searchEmail.orgaId === "number") {
-    query["info.organization_id"] = searchEmail.orgaId;
-  }
-
-  if (searchEmail.recipientEmail) {
-    query.recipientEmail = {
-      $options: "i",
-      $regex: new RegExp(`.*${searchEmail.recipientEmail}.*`),
-    };
-  }
-
-  if (searchEmail.territories) {
-    const tempQuery = { territories: {} };
-    parseTerritories(tempQuery, searchEmail, "territories", user);
-    query["info.territory"] = tempQuery.territories;
-  }
-
-  const nbResults = await countEmailsManage(query);
-
-  const options: QueryOptions = generateSearchOptions(
-    nbResults,
-    searchEmail.options
-  );
-
-  const emails = await findEmails(query, options);
-
-  return {
-    emails,
-    nbResults,
-  };
-};
-
-export const findOneEmail = async (_id: string) => {
-  return await findOne(_id);
-};
 
 // Campaign
 export const updateEmailStatus = async () => {
