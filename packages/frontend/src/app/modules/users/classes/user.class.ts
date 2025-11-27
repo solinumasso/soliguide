b257bc2f65f83d4854bb72739e71e8046ec66319
Kechiche Younes <younes@solinum.org>
2025-11-27 12:25:55 +0100

    fix(frontend): add areas to user class

--- packages/frontend/src/app/modules/users/classes/user.class.ts
+++ packages/frontend/src/app/modules/users/classes/user.class.ts
@@ -123,6 +123,11 @@ export class User {
     this.places = user?.places ?? [];
     this.currentOrga = null;
     this.token = null;
+    this.areas =
+      this.status === UserStatus.ADMIN_SOLIGUIDE ||
+      this.status === UserStatus.ADMIN_TERRITORY
+        ? user?.areas
+        : undefined;
 
     if (!light && user && this.pro && user.organizations?.length > 0) {
       this.organizations = user.organizations.map(
