7c17771d4efe2b2c0491dc031445bf1561c50828
Sophie <sophie.white@mordrid.net>
2026-01-28 13:59:18 +0100

    chore: deactivate translations

--- packages/frontend/src/app/modules/shared/pipes/decode-punycode-email.pipe.ts
+++ packages/frontend/src/app/modules/shared/pipes/decode-punycode-email.pipe.ts
@@ -22,12 +22,16 @@ import { Pipe, PipeTransform } from "@angular/core";
 import { toUnicode } from "punycode";
 
 export function decodePunycodeEmail(email: string): string {
+  if (!email) {
+    return "";
+  }
   const [local, domain] = email.split("@");
   return `${local}@${toUnicode(domain)}`;
 }
 
 @Pipe({ name: "decodePunycodeEmail" })
 export class DecodePunycodeEmailPipe implements PipeTransform {
+  // eslint-disable-next-line class-methods-use-this
   transform(email: string): string {
     return decodePunycodeEmail(email);
   }
