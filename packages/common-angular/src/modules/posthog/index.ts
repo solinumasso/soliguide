0d0e22288cb2fbfa137b14d74c429786b2e7331f
Yassine R. <riffi.yassine@gmail.com>
2026-02-06 01:42:28 +0100

    fix(common): upgrade posthog version

--- packages/common-angular/src/modules/posthog/index.ts
+++ packages/common-angular/src/modules/posthog/index.ts
@@ -21,6 +21,5 @@
 export * from "./posthog-add-user-id-headers.interceptor";
 export * from "./posthog-config";
 export * from "./posthog-properties.type";
-export * from "./posthog-router-init.service";
 export * from "./posthog.module";
 export * from "./posthog.service";
