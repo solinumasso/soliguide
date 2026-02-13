12cdffbe7d33959f1364b09584d43b9386ae7776
Yassine R. <riffi.yassine@gmail.com>
2026-02-13 21:46:18 +0100

    feat(frontend): add Last Login

--- packages/api/src/user/constants/FIELDS_TO_SELECT_FOR_SEARCH.const.ts
+++ packages/api/src/user/constants/FIELDS_TO_SELECT_FOR_SEARCH.const.ts
@@ -22,7 +22,7 @@ import { UserSearchContext } from "@soliguide/common";
 
 export const FIELDS_TO_SELECT_FOR_SEARCH: Record<UserSearchContext, string> = {
   MANAGE_PARTNERS:
-    "_id blocked areas categoriesLimitations createdAt developer devToken lastname mail name territories user_id verified",
+    "_id blocked areas categoriesLimitations createdAt developer devToken lastname lastLogin mail name territories user_id verified",
   MANAGE_USERS:
-    "_id createdAt areas invitations lastname mail name organizations passwordToken status territories translator user_id verified",
+    "_id createdAt areas invitations lastname lastLogin mail name organizations passwordToken status territories translator user_id verified",
 };
