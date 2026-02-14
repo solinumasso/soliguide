54a73283496e89ce15b4260a101b4e50589b905f
Yassine R. <riffi.yassine@gmail.com>
2026-02-15 00:27:24 +0100

    fix(common): update packages, update types, restore dockerfile

--- packages/soligare/src/pairing/services/pairing.service.ts
+++ packages/soligare/src/pairing/services/pairing.service.ts
@@ -195,7 +195,7 @@ export class PairingService {
     const connection = this.postgresService.getConnection();
 
     if (soliguide_id) {
-      await connection.begin(async (psql) => {
+      await connection.begin(async (psql: any) => {
         await psql`
           update
             dagster_structure.sources as src
--- packages/soligare/src/pairing/services/postgres.service.ts
+++ packages/soligare/src/pairing/services/postgres.service.ts
@@ -29,7 +29,7 @@ export class PostgresService implements OnApplicationShutdown {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private readonly connection: postgres.Sql<any>;
 
-  constructor(private configService: ConfigService) {
+  constructor(private readonly configService: ConfigService) {
     try {
       // skipcq: JS-0339
       const username: string = this.configService.get<string>(
@@ -75,7 +75,7 @@ export class PostgresService implements OnApplicationShutdown {
 
   async checkConnection() {
     try {
-      await this.connection.begin(async (sql) => {
+      await this.connection.begin(async (sql: any) => {
         await sql`SELECT NOW()`;
       });
     } catch (error) {
