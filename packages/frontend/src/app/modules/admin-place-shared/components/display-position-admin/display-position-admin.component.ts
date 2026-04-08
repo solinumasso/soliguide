e23234670116e82784635b1135ec7b047c042576
Sophie <sophie.white@mordrid.net>
2026-04-08 09:39:59 +0200

    chore(frontend): update conditionals

--- packages/frontend/src/app/modules/admin-place-shared/components/display-position-admin/display-position-admin.component.ts
+++ packages/frontend/src/app/modules/admin-place-shared/components/display-position-admin/display-position-admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { PlaceType } from "@soliguide/common";
 import { Place } from "../../../../models";
 
@@ -7,7 +7,30 @@ import { Place } from "../../../../models";
   templateUrl: "./display-position-admin.component.html",
   styleUrls: ["./display-position-admin.component.css"],
 })
-export class DisplayPositionAdminComponent {
+export class DisplayPositionAdminComponent implements OnInit {
   public readonly PlaceType = PlaceType;
   @Input() public place!: Place;
+  @Input() public oldPlace?: Place;
+
+  public addressChangeType: "added" | "modified" | null = null;
+  public additionalInfoChangeType: "added" | "modified" | "deleted" | null =
+    null;
+
+  public ngOnInit(): void {
+    if (!this.oldPlace) return;
+
+    const oldAddress = this.oldPlace.position?.address ?? "";
+    const newAddress = this.place.position?.address ?? "";
+    if (oldAddress !== newAddress) {
+      this.addressChangeType = oldAddress ? "modified" : "added";
+    }
+
+    const oldInfo = this.oldPlace.position?.additionalInformation ?? "";
+    const newInfo = this.place.position?.additionalInformation ?? "";
+    if (oldInfo !== newInfo) {
+      if (!oldInfo && newInfo) this.additionalInfoChangeType = "added";
+      else if (oldInfo && !newInfo) this.additionalInfoChangeType = "deleted";
+      else this.additionalInfoChangeType = "modified";
+    }
+  }
 }
