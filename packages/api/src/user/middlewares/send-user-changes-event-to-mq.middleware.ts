93bff91a724e1b9b500a61416272e3d14d654d2e
Sophie White <125132533+SElizaWhi@users.noreply.github.com>
2026-04-13 09:17:54 +0200

    feat(api): Sync users to brevo (#1243)

--- packages/api/src/user/middlewares/send-user-changes-event-to-mq.middleware.ts
+++ packages/api/src/user/middlewares/send-user-changes-event-to-mq.middleware.ts
@@ -10,14 +10,21 @@ import {
   amqpEventsSender,
   AmqpSynchroAirtableUserEvent,
 } from "../../events";
+import { getUserRightsWithParams } from "../services";
 
 export const sendUserChangesToMq = async (
   req: ExpressRequest & {
-    isUserDeleted: boolean;
-    updatedUser: UserPopulateType;
+    isUserDeleted?: boolean;
+    updatedUser?: UserPopulateType;
   }
 ) => {
   if (req.updatedUser) {
+    if (!req.updatedUser.userRights?.length) {
+      req.updatedUser.userRights = await getUserRightsWithParams({
+        user: req.updatedUser._id,
+      });
+    }
+
     const payload = new AmqpSynchroAirtableUserEvent(
       req.updatedUser,
       req.requestInformation.frontendUrl,
@@ -34,15 +41,17 @@ export const sendUserChangesToMq = async (
   }
 };
 
-export const sendUserChangesToMqAndNext = async (
+export const sendUserChangesToMqAndNext = (
   req: ExpressRequest & {
-    isUserDeleted: boolean;
-    updatedUser: UserPopulateType;
+    isUserDeleted?: boolean;
+    updatedUser?: UserPopulateType;
   },
   _res: ExpressResponse,
   next: NextFunction
 ) => {
-  sendUserChangesToMq(req);
+  sendUserChangesToMq(req).catch((e) =>
+    req.log.error(e, "Failed to send user changes to MQ")
+  );
 
   return next();
 };
