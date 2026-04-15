8b563218472ebabb666098f0a741bc01e9eb350b
UlrichFrancomme <ulrich.francomme@gmail.com>
2026-04-16 02:07:17 +0400

    refactor(api): export some models and utils to be reused by public-api

---
+++ packages/api/src/index.ts
@@ -0,0 +1,4 @@
+export { PlaceModel } from "./place/models/place.model";
+export { UserModel } from "./user/models/user.model";
+export { FIELDS_FOR_SEARCH } from "./search/constants/requests/FIELDS_FOR_SEARCH.const";
+export { getServiceCategoriesApi } from "./categories/functions/get-service-categories-api.function";
