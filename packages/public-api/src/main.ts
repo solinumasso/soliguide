e4086ae9a587fa3e3f54ea7360e89d3d601943e2
UlrichFrancomme <ulrich.francomme@gmail.com>
2026-04-16 02:09:04 +0400

    feat(public-api): wire app

--- packages/public-api/src/main.ts
+++ packages/public-api/src/main.ts
@@ -5,9 +5,30 @@ import {
 } from "@nestjs/platform-fastify";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import helmet from "helmet";
+import { PlaceModel } from "@soliguide/api";
 import { AppModule } from "./app.module";
 
+import { config } from "dotenv";
+
+config();
+
 (async () => {
+  const mongodbUri = process.env.MONGODB_URI;
+  const jwtSecret = process.env.JWT_SECRET;
+
+  if (!jwtSecret) {
+    throw new Error("JWT_SECRET is required");
+  }
+
+  if (mongodbUri) {
+    await PlaceModel.db.base.connect(mongodbUri, {
+      maxIdleTimeMS: 50_000,
+      serverSelectionTimeoutMS: 5_000,
+    });
+  } else {
+    throw new Error("MONGODB_URI is required");
+  }
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
@@ -25,6 +46,7 @@ import { AppModule } from "./app.module";
   SwaggerModule.setup("doc", app, document);
 
   app.use(helmet());
+  app.enableShutdownHooks();
 
   await app.listen(process.env.PORT ?? 3002, "0.0.0.0");
 })();
