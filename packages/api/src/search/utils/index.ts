d995c35d5157409339f10b39718080050f178f36
Kechiche Younes <younes@solinum.org>
2026-04-21 12:56:25 +0200

    feat(icons-generator): add neurology and vascular_surgery icons

--- packages/api/src/search/utils/index.ts
+++ packages/api/src/search/utils/index.ts
@@ -1,6 +1,5 @@
 // @index('./*', f => `export * from '${f.path}'`)
 export * from "./convertPlaceFromNewMobilityToOld";
 export * from "./convertPlaceFromNewHealthToOld";
-export * from "./getLangsForCountry";
 export * from "./parsers";
 export * from "./query";
