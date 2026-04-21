026694cd6a966096402c845f5c20ff9f2603738d
Kechiche Younes <younes@solinum.org>
2026-04-21 17:47:19 +0200

    feat(common): remove filter of addiction services into manage

--- packages/common/src/search-places/constants/DEFAULT_SERVICES_TO_EXCLUDE_WITH_ADDICTION.ts
+++ packages/common/src/search-places/constants/DEFAULT_SERVICES_TO_EXCLUDE_WITH_ADDICTION.ts
@@ -1,7 +1,5 @@
-import { Categories } from "../../categories";
 import { DEFAULT_SERVICES_TO_EXCLUDE } from "./DEFAULT_SERVICES_TO_EXCLUDE.const";
 
 export const DEFAULT_SERVICES_TO_EXCLUDE_WITH_ADDICTION = [
-  Categories.ADDICTIONS,
   ...DEFAULT_SERVICES_TO_EXCLUDE,
 ];
