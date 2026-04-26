0fc362b3233e2c555404e3a349419ccc70286dd2
UlrichFrancomme <ulrich.francomme@gmail.com>
2026-04-26 18:30:57 +0400

    fix(public-api): schema open api and correct typings

--- packages/public-api/src/app.module.ts
+++ packages/public-api/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
+
 import { SearchModule } from "./search/search.module";
 
 @Module({
   imports: [SearchModule],
-  controllers: [AppController],
 })
 export class AppModule {}
