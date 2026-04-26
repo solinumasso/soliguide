d492a0772d61f3bf190d968ba25dc10ced4a404f
Yassine R. <riffi.yassine@gmail.com>
2026-04-26 22:51:04 +0200

    fix(web-app): add maintenance page

---
+++ packages/frontend/src/app/modules/general/components/solidata-maintenance/solidata-maintenance.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-solidata-maintenance",
+  templateUrl: "./solidata-maintenance.component.html",
+  styleUrls: ["./solidata-maintenance.component.css"],
+})
+export class SolidataMaintenanceComponent {}
