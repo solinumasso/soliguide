14075b271b6681e6af9f716d7ac43ee5e0caf494
UlrichFrancomme <ulrich.francomme@gmail.com>
2026-04-29 00:25:07 +0400

    feat(public-api): api runtime version migrations

---
+++ packages/public-api/src/versioning-engine/runtime-pipeline/index.ts
@@ -0,0 +1,4 @@
+export { DowngradePipeline } from "./downgrade.pipeline";
+export { UpgradePipeline } from "./upgrade.pipeline";
+export * from "./version-path.resolver";
+export * from "./transformers";
