a04ad0f7697fe68a405ef74c2d97bc1852341700
Kechiche Younes <younes@solinum.org>
2026-06-02 15:41:34 +0200

    feat: prefix 3rd level services in export and in place view

--- packages/api/src/autoexport/services/parsers/parse-section-name.parse.ts
+++ packages/api/src/autoexport/services/parsers/parse-section-name.parse.ts
@@ -1,11 +1,17 @@
-import { ApiPlace, SortingFilters } from "@soliguide/common";
+import {
+  ApiPlace,
+  CategoriesService,
+  CommonNewPlaceService,
+  SortingFilters,
+} from "@soliguide/common";
 import { ExportSearchParams } from "../../interfaces/ExportSearchParams.interface";
 import { translateServiceName } from "./parse-services-categories";
 
 export const parseSectionName = (
   searchData: ExportSearchParams,
   place: Pick<ApiPlace, "position" | "services_all">,
-  service?: any
+  service?: CommonNewPlaceService,
+  categoriesService?: CategoriesService
 ): string => {
   if (searchData.exportParams.sortingFilter === SortingFilters.CITY) {
     return place.position?.city.trim() ?? "";
@@ -13,13 +19,28 @@ export const parseSectionName = (
   if (searchData.exportParams.sortingFilter === SortingFilters.POSTAL_CODE) {
     return place.position?.postalCode.trim() ?? "";
   }
-  // For draft places whitout services
-  if (!service) {
+  // For draft places without services
+  if (!service?.category) {
     return "";
   }
 
-  return translateServiceName(
+  const serviceName = translateServiceName(
     service.category,
     searchData.exportParams.language
   );
+
+  if (categoriesService) {
+    const parentCategory = categoriesService.getParentCategoryIfNeedPrefix(
+      service.category
+    );
+    if (parentCategory) {
+      const parentName = translateServiceName(
+        parentCategory,
+        searchData.exportParams.language
+      );
+      return `${parentName}: ${serviceName}`;
+    }
+  }
+
+  return serviceName;
 };
