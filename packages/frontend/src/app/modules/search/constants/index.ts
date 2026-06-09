a406d249637d3fbe6274f32db0ac70b2be28e4d9
Kechiche Younes <younes@solinum.org>
2026-06-09 22:36:35 +0200

    feat: use suggestionType instead of building trackingData object for posthog tracking

--- packages/frontend/src/app/modules/search/constants/index.ts
+++ packages/frontend/src/app/modules/search/constants/index.ts
@@ -1,4 +1,3 @@
 //@index('./*', f => `export * from '${f.path}'`)
-export * from "./DEFAULT_TRACKING_DATA.const";
 export * from "./RESULTS_SEARCH_LIMIT.const";
 export * from "./SEARCH_OPTIONAL_PARAMS.const";
--- packages/frontend/src/app/modules/shared/components/search-category-autocomplete/search-category-autocomplete.component.ts
+++ packages/frontend/src/app/modules/shared/components/search-category-autocomplete/search-category-autocomplete.component.ts
@@ -262,7 +262,8 @@ export class SearchCategoryAutocompleteComponent
   ) {
     this.search.category = category;
     this.search.word = null;
-    this.search.trackingData = { organization: null, typeOfPlace: null };
+    this.search.suggestionType = AutoCompleteType.CATEGORY;
+    this.search.suggestionValue = null;
     this.search.label = this.translateService.instant(category.toUpperCase());
     this.updateCategory.emit();
     this.captureEvent(`click-autocomplete-search-category-${category}`, {
@@ -278,14 +279,8 @@ export class SearchCategoryAutocompleteComponent
     this.search.category = null;
     this.search.word = item?.slug ?? slugString(item?.label);
     this.search.label = item?.label;
-    this.search.trackingData = {
-      organization:
-        item.type === AutoCompleteType.ORGANIZATION ? item.slug ?? null : null,
-      typeOfPlace:
-        item.type === AutoCompleteType.ESTABLISHMENT_TYPE
-          ? item.slug ?? null
-          : null,
-    };
+    this.search.suggestionType = item.type;
+    this.search.suggestionValue = item.slug ?? null;
     this.updateCategory.emit();
     this.captureEvent(`click-autocomplete-search-word`, {
       keyUsed,
