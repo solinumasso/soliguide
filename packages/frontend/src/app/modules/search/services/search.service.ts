c4db8287c98b5ca6111c3a45034015a3e338fcb9
UlrichFrancomme <ulrich@francomme.re>
2026-08-25 13:59:03 +0400

    feat(frontend): use new public-api endpoints for search

--- packages/frontend/src/app/modules/search/services/search.service.ts
+++ packages/frontend/src/app/modules/search/services/search.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 import { ApiSearchResults, SearchResults } from "@soliguide/common";
@@ -6,7 +6,6 @@ import { ApiSearchResults, SearchResults } from "@soliguide/common";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { Search } from "../interfaces";
-import { CurrentLanguageService } from "../../general/services/current-language.service";
 import { Place } from "../../../models";
 
 import { environment } from "../../../../environments/environment";
@@ -15,35 +14,47 @@ import { environment } from "../../../../environments/environment";
   providedIn: "root",
 })
 export class SearchService {
-  public endPoint = `${environment.apiUrl}/new-search`;
+  public endPoint = `${environment.publicApiUrl}/search`;
 
-  constructor(
-    private readonly http: HttpClient,
-    private readonly currentLanguageService: CurrentLanguageService
-  ) {}
+  constructor(private readonly http: HttpClient) {}
 
   public launchSearch(search: Search): Observable<SearchResults<Place>> {
-    const url = `${this.endPoint}/${this.currentLanguageService.currentLanguage}`;
+    return this.http
+      .post<ApiSearchResults>(
+        this.endPoint,
+        this.toPublicApiSearchRequest(search),
+        {
+          headers: new HttpHeaders({ "x-api-version": "2026-01-01" }),
+        }
+      )
+      .pipe(
+        map((response: ApiSearchResults) => {
+          const result: SearchResults<Place> = {
+            nbResults: 0,
+            results: [],
+          };
 
-    return this.http.post<ApiSearchResults>(`${url}`, search).pipe(
-      map((response: ApiSearchResults) => {
-        const result: SearchResults<Place> = {
-          nbResults: 0,
-          results: [],
-        };
+          if (!response.nbResults) {
+            return result;
+          }
 
-        if (!response.nbResults) {
+          if (response.nbResults > 0) {
+            result.nbResults = response.nbResults;
+            result.results = response.places.map(
+              (item) => new Place(item, false)
+            );
+          }
           return result;
-        }
+        })
+      );
+  }
 
-        if (response.nbResults > 0) {
-          result.nbResults = response.nbResults;
-          result.results = response.places.map(
-            (item) => new Place(item, false)
-          );
-        }
-        return result;
-      })
-    );
+  private toPublicApiSearchRequest(search: Search): Record<string, unknown> {
+    const { languages, ...request } = search;
+
+    return {
+      ...request,
+      ...(languages ? { languages: [languages] } : {}),
+    };
   }
 }
