2ae511d066717329ade67bbe329db8a70acfe302
UlrichFrancomme <ulrich@francomme.re>
2026-08-31 19:36:19 +0400

    build(public-api): fix dependencies

--- packages/public-api/src/versioning-engine/index.ts
+++ packages/public-api/src/versioning-engine/index.ts
@@ -3,4 +3,3 @@ export * from "./open-api/version-registry";
 export * from "./versioning.module";
 export * from "./runtime-pipeline";
 export * from "./open-api/open-api.generator";
-export * from "./schema-versioning/schema-version.generator";
